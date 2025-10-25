886b27dab6a28f41888626711d89f7288d6dfacc
SANJAYintrinsic <sanjay.s261004@gmail.com>
2025-10-25 17:02:55 +0530

    Create swapping.java

---
+++ swapping.java
@@ -0,0 +1,13 @@
+public class SwapFunction {
+    static void swap(int a, int b) {
+        System.out.println("Before swap: a = " + a + ", b = " + b);
+        int temp = a;
+        a = b;
+        b = temp;
+        System.out.println("After swap: a = " + a + ", b = " + b);
+    }
+
+    public static void main(String[] args) {
+        swap(5, 9);
+    }
+}
