a0a2ff3d7625877469e4c8c0fa3170d26daac8b8
SANJAYintrinsic <sanjay.s261004@gmail.com>
2025-10-25 06:51:12 +0530

    Create PowerOfNum.java

---
+++ PowerOfNum.java
@@ -0,0 +1,10 @@
+public class practice {
+    public static void main(String[] args) {
+        int base = 2;
+        int exponent = 5;
+        double result =(int) Math.pow(base, exponent);
+        
+
+        System.out.println(base + " raised to the power of " + exponent + " is: " + result);
+    }
+}
