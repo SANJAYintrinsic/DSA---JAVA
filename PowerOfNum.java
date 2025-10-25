9ce09e51ac9d04b7b8a5bfc7a24a1ce150272df3
SANJAYintrinsic <sanjay.s261004@gmail.com>
2025-10-25 17:47:26 +0530

    Update PowerOfNum.java

--- PowerOfNum.java
+++ PowerOfNum.java
@@ -2,7 +2,17 @@ public class practice {
     public static void main(String[] args) {
         int base = 2;
         int exponent = 5;
-        double result =(int) Math.pow(base, exponent);
+
+Math.max(a, b);
+Math.min(a, b);
+Math.abs(x);
+Math.sqrt(x);
+Math.cbrt(x);
+Math.pow(a, b);
+Math.toDegrees(x);
+Math.toRadians(x);
+Math.random();                         
+(int)(Math.random() * n);  
         
 
         System.out.println(base + " raised to the power of " + exponent + " is: " + result);
