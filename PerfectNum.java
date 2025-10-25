23cca0b5119c4c7fb5e3d49eb89db92a462b5cff
SANJAYintrinsic <sanjay.s261004@gmail.com>
2025-10-25 07:22:00 +0530

    Create PerfectNum.java

---
+++ PerfectNum.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class PerfectNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        
+        int sum = 0;
+
+        // Find divisors and sum them
+        for (int i = 1; i <= num / 2; i++) {
+            if (num % i == 0) {
+                sum += i;
+            }
+        }
+
+        // Check if it's perfect
+        if (sum == num) {
+            System.out.println(num + " is a perfect number.");
+        } else {
+            System.out.println(num + " is not a perfect number.");
+        }
+
+        sc.close();
+    }
+}
