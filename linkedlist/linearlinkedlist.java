716d17096b0251d7acbf71903fd13f8d67da84d1
SANJAYintrinsic <sanjay.s261004@gmail.com>
2026-01-01 18:57:59 +0530

    Create linearlinkedlist.java

---
+++ linkedlist/linearlinkedlist.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class LinkedListInput {
+
+    // Function to insert node at end
+    static Node insertAtEnd(Node head, int data) {
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            return newNode;
+        }
+
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = newNode;
+
+        return head;
+    }
+
+    // Function to display linked list
+    static void display(Node head) {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Node head = null;
+
+        System.out.print("Enter number of nodes: ");
+        int n = sc.nextInt();
+
+        System.out.println("Enter node values:");
+        for (int i = 0; i < n; i++) {
+            int value = sc.nextInt();
+            head = insertAtEnd(head, value);
+        }
+
+        System.out.println("Linear Linked List:");
+        display(head);
+
+        sc.close();
+    }
+}
