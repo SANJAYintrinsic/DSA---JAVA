fdeea5b6fddda6190ba6eb3f44f328f976d5705e
Sanjay S <sanjay.s261004@gmail.com>
2026-05-14 16:58:33 +0530

    Update ArrayLargeSmall.java

--- Array/ArrayLargeSmall.java
+++ Array/ArrayLargeSmall.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.util.*;
 
-class arrbigone{
+class arrbigone{            //    T - O(N)   S - O(N)
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
 
