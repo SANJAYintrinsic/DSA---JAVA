239ea920d773f80e8b7cf12db7396ef1f65cea7f
Sanjay S <sanjay.s261004@gmail.com>
2026-05-18 14:40:14 +0530

    Update SortCheck.java

--- Array/SortCheck.java
+++ Array/SortCheck.java
@@ -1,23 +1,23 @@
 import java.util.Scanner;
 import java.util.*;
 
-class sortCheck{       // T- O(N)  S- O(N)
+class sortCheck{
     public static void main(String[] args) {
-        int[] arr={1,2,8,4,5};
+        int[] arr={1,2,3,4,5,6,7,8,9,10};
         boolean sort=true;
 
-        for(int i=1;i<5;i++){
-            if(arr[i]<arr[i-1]){
+        for(int i=1;i<arr.length-1;i++){
+            if(arr[i]>arr[i+1]){
                 sort=false;
                 break;
             }
         }
 
         if(sort){
-            System.out.print("Sorted array");
+            System.out.print("sorted");
         }
         else{
-            System.out.print("Unsorted array");
+            System.out.println("Not sorted");
         }
     }
 }
