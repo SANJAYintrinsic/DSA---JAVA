f815d492187457726ec52cd863685bca373081f4
Sanjay S <sanjay.s261004@gmail.com>
2026-05-19 19:16:27 +0530

    Update Binarysearch.java

--- Array/Binarysearch.java
+++ Array/Binarysearch.java
@@ -1,33 +1,29 @@
 import java.util.*;
+import java.util.Scanner;
 
-class BinarySearch {
-    public static void main(String[] args) {
+                          // O(log n)
+                          // Sorted Array
+                          // mid=start+(end-start) /2
 
-        int[] arr = {1, 3, 5, 7, 9, 11, 15};
+class Binarysearch{
+    public static void main(String[] args){
+        int[] arr={1,2,3,4,5,6,7,8};
+        int target=8;
+        int start=0;
+        int end=arr.length-1;
 
-        int target = 9;
-
-        int low = 0;
-        int high = arr.length - 1;
-
-        while(low <= high) {
-
-            // Safer mid calculation
-            int mid = low + (high - low) / 2;
-
-            if(arr[mid] == target) {
-                System.out.println("Element found at index: " + mid);
-                return;
+        while(start<=end){
+            int mid=start+(end-start) /2;
+            if(arr[mid]==target){
+                System.out.print(mid);
+                break;
             }
-
-            if(arr[mid] < target) {
-                low = mid + 1;
+            else if(arr[mid]<target){
+                start=mid+1;
             }
-            else {
-                high = mid - 1;
+            else if(arr[mid]>target){
+                end=mid-1;
             }
         }
-
-        System.out.println("Element not found");
     }
 }
