43bd6418f65d0a07cd7244d9050ffa30cf30c2a9
Sanjay S <sanjay.s261004@gmail.com>
2026-05-23 21:14:57 +0530

    Update binaryTree.java

--- Tree/binaryTree.java
+++ Tree/binaryTree.java
@@ -1,1 +1,30 @@
+class Node{
+    int data;
+    Node left,right;
+    Node(int data){
+        this.data=data;
+        left=right=null;
+    }
+}
 
+class binarytree{
+    Node root=null;
+    void order(Node node){
+        if(node==null){
+            return;
+        }
+        System.out.print(node.data+" ");
+        order(node.left);
+        order(node.right);
+    }
+
+    public static void main(String[] args){
+        binarytree tree=new binarytree();
+        tree.root=new Node(1);
+        tree.root.left=new Node(2);
+        tree.root.right=new Node(3);
+        tree.root.left.left=new Node(4);
+        tree.root.right.right=new Node(6);
+        tree.order(tree.root);
+    }
+}
