f2c57a122e26057bee1b02818a562babdd46718b
Sanjay S <sanjay.s261004@gmail.com>
2026-05-26 11:36:35 +0530

    Update reverse.java

--- Stack/reverse.java
+++ Stack/reverse.java
@@ -1,1 +1,25 @@
 
+import java.util.*;
+
+class StackReverse {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        Stack<Integer> stack = new Stack<>();
+
+        // Input 5 elements
+        for (int i = 0; i < 5; i++) {
+            stack.push(sc.nextInt());
+        }
+
+        Stack<Integer> reverse = new Stack<>();
+
+        // Reverse stack
+        while (!stack.isEmpty()) {
+            reverse.push(stack.pop());
+        }
+
+        System.out.println(reverse);
+    }
+}
