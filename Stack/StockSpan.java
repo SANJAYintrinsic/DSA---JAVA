46df5590df33368d7ee1332c31fc70403a0926ee
Sanjay S <sanjay.s261004@gmail.com>
2026-05-27 15:58:11 +0530

    Create StockSpan.java

---
+++ Stack/StockSpan.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class stockspan{
+
+    public static int[] stock(int[] price){
+        int n=price.length;
+        int[] span=new int[n];
+        Stack<Integer> stack=new Stack<>();
+
+        stack.push(0);
+        span[0]=1;
+
+        for(int i=1;i<n;i++){
+            while (!stack.isEmpty() && price[stack.peek()]<=price[i]) {
+                stack.pop();
+            }
+
+            if(stack.isEmpty()){
+                span[i]=i+1;
+            }
+            else{
+                span[i]=i-stack.peek();
+            }
+
+            stack.push(i);
+        }
+
+        return span;
+    }
+    public static void main(String[] args) {
+        int[] price={100, 80, 60, 70, 60, 75, 85};
+        int[] max=stock(price);
+        for(int num:max){
+            System.out.print(num+" ");
+        }
+    }
+}
