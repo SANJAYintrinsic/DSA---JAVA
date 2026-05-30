b93572db2d1de6198f9494f4cd004568a8ce2300
Sanjay S <sanjay.s261004@gmail.com>
2026-05-30 15:01:05 +0530

    Create StringMethods.java

---
+++ String/StringMethods.java
@@ -0,0 +1,16 @@
+A String in Java is a sequence of characters used to store text.\
+
+String str = "Hello";
+
+String str = new String("Hello");  //Creates a new object in HEAP MEMORY.
+
+s.length()
+s.charAt(2)
+s.substring(5)
+s.toUpperCase()
+s.toLowerCase()
+s.equals("Java")
+s.equalsIgnoreCase("java")
+s.contains("Prog")
+s.replace('a','x')
+s.trim()
