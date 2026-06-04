4fa78ec89447e2ee709933a3585ad8b01c7c770a
Sanjay S <sanjay.s261004@gmail.com>
2026-06-04 10:47:05 +0530

    Update RemoveDuplicate.java

--- Array/RemoveDuplicate.java
+++ Array/RemoveDuplicate.java
@@ -1,24 +1,31 @@
-import java.util.*;
-
-class Array{
-    public static void main(String[] args) {
-        int[] arr={8,4,2,5,6,1,8,9,6,4,2};
-        int n=arr.length;
 
         //  T.C - O(N)  S.C - O(N)
-        
-        HashSet<Integer> hs=new HashSet<Integer>();  
-
         // LinkedHashSet<Integer> set=new LinkedHashSet<>();
         // original order of array unique elements
 
-        for(int i=0;i<n;i++){
-            hs.add(arr[i]);
+import java.util.*;
+
+class Duplicate {
+    public static void main(String[] args) {
+        int[] arr = {4, 2, 3, 2, 1, 6, 6, 6, 6};
+
+        LinkedHashSet<Integer> duplicate = new LinkedHashSet<>();
+        LinkedHashSet<Integer> hs = new LinkedHashSet<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            if (!hs.add(arr[i])) {   // add() returns false if already present
+                duplicate.add(arr[i]);
+            }
         }
 
-        for(int value:hs){
-        System.out.print(value+" ");
+        System.out.print("Unique elements: ");
+        for (int n : hs) {
+            System.out.print(n + " ");
         }
 
+        System.out.print("\nDuplicate elements: ");
+        for (int num : duplicate) {
+            System.out.print(num + " ");
+        }
     }
 }
