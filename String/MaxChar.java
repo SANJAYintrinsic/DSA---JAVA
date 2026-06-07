fada589528cf81fdfbfed56c83c5a69e90200c3e
Sanjay S <sanjay.s261004@gmail.com>
2026-06-07 17:25:12 +0530

    Create MaxChar.java

---
+++ String/MaxChar.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Maxchar{
+    public static void main(String[] args){
+        String str="All is Well";
+        str=str.toLowerCase();
+        HashMap<Character,Integer> map=new HashMap<>();
+        for(char ch:str.toCharArray()){
+            if(ch!=' '){
+            map.put(ch,map.getOrDefault(ch,0)+1);
+            }
+        }
+
+        int maxcount=0;
+        char maxelement='\0';
+
+        for(Map.Entry<Character,Integer> entry:map.entrySet()){
+            if(entry.getValue()>maxcount){
+                maxcount=entry.getValue();
+                maxelement=entry.getKey();
+            }
+        }
+
+        System.out.print(maxelement+"-"+maxcount);
+    }
+}
