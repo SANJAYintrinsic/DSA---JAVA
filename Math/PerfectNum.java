6fa5bfffaee35ed3f382e6290e83747c1c1a9d49
Sanjay S <sanjay.s261004@gmail.com>
2026-06-08 15:34:26 +0530

    Update PerfectNum.java

--- Math/PerfectNum.java
+++ Math/PerfectNum.java
@@ -3,24 +3,22 @@ import java.util.Scanner;
 public class PerfectNumber {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+
         System.out.print("Enter a number: ");
         int num = sc.nextInt();
-        
+
         int sum = 0;
 
-        // Find divisors and sum them
         for (int i = 1; i <= num / 2; i++) {
             if (num % i == 0) {
                 sum += i;
             }
         }
 
-        // Check if it's perfect
         if (sum == num) {
-            System.out.println(num + " is a perfect number.");
+            System.out.println(num + " is a Perfect Number");
         } else {
-            System.out.println(num + " is not a perfect number.");
+            System.out.println(num + " is not a Perfect Number");
         }
 
         sc.close();
