4841d8b66b0f6b7a3c19766d43599af2be4345af
Sanjay S <sanjay.s261004@gmail.com>
2026-07-07 18:33:28 +0530

    Create decimal.java

---
+++ Math/decimal.java
@@ -0,0 +1,14 @@
+float f = 1.123456789f;
+double d = 1.123456789;
+
+System.out.println(f);
+System.out.println(d);
+
+output:
+1.1234568
+1.123456789
+
+  
+double average = 3.6666666;
+
+System.out.printf("%.5f", average);
