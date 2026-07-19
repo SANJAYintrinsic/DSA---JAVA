71dd127962168535f8d4033b1d5355bb823f0c4f
Sanjay S <sanjay.s261004@gmail.com>
2026-07-19 15:44:09 +0530

    Create binary.java

---
+++ Array/binary.java
@@ -0,0 +1,10 @@
+String binaryString = "1101";
+int number = Integer.parseInt(binaryString, 2);
+
+System.out.println(number); // Output: 13
+
+
+int number = 13;
+String binaryString = Integer.toBinaryString(number);
+
+System.out.println(binaryString); // Output: 1101
