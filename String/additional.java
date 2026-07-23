6841ae52bf13a97011fb4ee6f1f166cb01802fb7
Sanjay S <sanjay.s261004@gmail.com>
2026-07-23 13:29:38 +0530

    Update additional.java

--- String/additional.java
+++ String/additional.java
@@ -20,3 +20,50 @@ public static void main(String[] args){
         int total=star-hash;
         System.out.print("No of strings needed : "+total);
 }
+
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // Read number of balloons
+        int N = sc.nextInt();
+
+        // Array to store balloon colors
+        String[] B = new String[N];
+
+        // HashMap to store frequency
+        HashMap<String, Integer> map = new HashMap<>();
+
+        // Read balloon colors and count frequency
+        for (int i = 0; i < N; i++) {
+            B[i] = sc.next();
+
+            if (map.containsKey(B[i])) {
+                map.put(B[i], map.get(B[i]) + 1);
+            } else {
+                map.put(B[i], 1);
+            }
+        }
+
+        // Find the first color with odd frequency
+        boolean found = false;
+
+        for (int i = 0; i < N; i++) {
+            if (map.get(B[i]) % 2 != 0) {
+                System.out.println(B[i]);
+                found = true;
+                break;
+            }
+        }
+
+        // If no odd frequency found
+        if (!found) {
+            System.out.println("All are even");
+        }
+    }
+}
