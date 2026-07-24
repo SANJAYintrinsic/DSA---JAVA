2ce74f985409333982f34fb772a27e8967eceab1
Sanjay S <sanjay.s261004@gmail.com>
2026-07-24 16:49:37 +0530

    Create ASCII.java

---
+++ String/ASCII.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class sumarr {
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+
+        char ch='a';
+        int n=(int) ch;
+        System.out.println(n);
+    }
+}
