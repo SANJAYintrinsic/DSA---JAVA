5c9a05024509b93b7003ac64963e8f9269054d8d
Sanjay S <sanjay.s261004@gmail.com>
2026-07-24 17:15:43 +0530

    Update Rotate.java

--- Array/Rotate.java
+++ Array/Rotate.java
@@ -35,3 +35,48 @@ class Rotate{               // T - O(N)  S - O(D)
         }
     }
 }
+
+
+import java.util.*;
+
+class Rotate {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the size: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter element: ");
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter rotation: ");
+        int d = sc.nextInt();
+
+        d = d % n;
+
+        // Store last d elements
+        int[] temp = new int[d];
+        for (int i = 0; i < d; i++) {
+            temp[i] = arr[n - d + i];
+        }
+
+        // Shift remaining elements to the right
+        for (int i = n - d - 1; i >= 0; i--) {
+            arr[i + d] = arr[i];
+        }
+
+        // Copy temp to beginning
+        for (int i = 0; i < d; i++) {
+            arr[i] = temp[i];
+        }
+
+        // Print array
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
