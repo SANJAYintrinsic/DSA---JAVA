3471e5f4f774cdda87f96268ffad5d445c2426f5
Sanjay S <sanjay.s261004@gmail.com>
2026-07-25 14:00:28 +0530

    Update and rename 3Sum.java to TwoSums.java

--- Array/TwoSums.java
+++ Array/TwoSums.java
@@ -3,6 +3,38 @@ import java.util.*;
 public class sumarr {
     public static void main(String[] args) {
 
+        int[] arr = {1, 7, 5, 2, 9, 4, 5, 6};
+        int n = arr.length;
+        int target = 8;
+        boolean found = false;
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+
+                if (arr[i] + arr[j] == target) {
+                    System.out.println(i + " " + j);
+                    found = true;
+                    break;
+                }
+            }
+
+            if (found) {
+                break;
+            }
+        }
+
+        if (!found) {
+            System.out.println("No");
+        }
+    }
+}
+
+
+import java.util.*;
+
+public class sumarr {
+    public static void main(String[] args) {
+
         Scanner sc=new Scanner(System.in);
 
         int[] arr={1,7,5,2,9,4,5,6};
