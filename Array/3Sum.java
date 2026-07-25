80c23f5aba599ca5c63b7db4288af14209704b97
Sanjay S <sanjay.s261004@gmail.com>
2026-07-25 13:54:53 +0530

    Update 3Sum.java

--- Array/3Sum.java
+++ Array/3Sum.java
@@ -1,18 +1,79 @@
-public class Main {
+import java.util.*;
+
+public class sumarr {
     public static void main(String[] args) {
 
-        int[] arr = {-1, 0, 1, 2, -1, -4};
-        int n = arr.length;
+        Scanner sc=new Scanner(System.in);
 
-        for (int i = 0; i < n - 2; i++) {
-            for (int j = i + 1; j < n - 1; j++) {
-                for (int k = j + 1; k < n; k++) {
+        int[] arr={1,7,5,2,9,4,5,6};
+        int n=arr.length;
+        int target=8;
+        boolean found=false;
 
-                    if (arr[i] + arr[j] + arr[k] == 0) {
-                        System.out.println(arr[i] + " " + arr[j] + " " + arr[k]);
+        for(int i=0;i<n-2;i++){
+            for(int j=i+1;j<n-1;j++){
+                for(int k=j+1;k<n;k++){
+                    if(arr[i]+arr[j]+arr[k]==target){
+                        System.out.print(i+" "+j+" "+k);
+                        found=true;
+                        break;
                     }
                 }
+                if(found){
+                    break;
+                }
             }
+            if(found){
+                break;
+            }
+        }
+
+        if(!found){
+            System.out.print("No");
+        }
+    }
+}
+
+
+
+
+
+import java.util.*;
+
+public class sumarr {
+    public static void main(String[] args) {
+
+        int[] arr = {1, 7, 5, 2, 9, 4, 5, 6};
+        int n = arr.length;
+        int target = 15;
+        boolean found = false;
+
+        for (int i = 0; i < n - 3; i++) {
+            for (int j = i + 1; j < n - 2; j++) {
+                for (int k = j + 1; k < n - 1; k++) {
+                    for (int l = k + 1; l < n; l++) {
+
+                        if (arr[i] + arr[j] + arr[k] + arr[l] == target) {
+                            System.out.println(i + " " + j + " " + k + " " + l);
+                            found = true;
+                            break;
+                        }
+                    }
+
+                    if (found)
+                        break;
+                }
+
+                if (found)
+                    break;
+            }
+
+            if (found)
+                break;
+        }
+
+        if (!found) {
+            System.out.println("No");
         }
     }
 }
