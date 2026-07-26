2b97945f969932903b19205617a4bf766f10e571
Sanjay S <sanjay.s261004@gmail.com>
2026-07-26 16:56:02 +0530

    Update binary.java

--- Array/binary.java
+++ Array/binary.java
@@ -29,6 +29,24 @@ public class SwapNibbles {
 }
 
 
+float f = 1.123456789f;
+double d = 1.123456789;
+
+System.out.println(f);
+System.out.println(d);
+
+output:
+1.1234568
+1.123456789
+
+  
+double average = 3.6666666;
+
+System.out.printf("%.5f", average);
+
+
+
+
 int num=178;
         String binary = Integer.toBinaryString(num);
         String result = "";
