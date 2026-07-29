3fd2279c3fa4e1915550dff3e0d200302ee6cd36
Sanjay S <sanjay.s261004@gmail.com>
2026-07-29 16:11:25 +0530

    Create MaxMovesForConsecutive.java

---
+++ Array/MaxMovesForConsecutive.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class sumarr {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        Arrays.sort(arr);
+
+        int left = 0;
+        int maxWindow = 0;
+
+        for (int right = 0; right < n; right++) {
+
+            while (arr[right] - arr[left] >= n) {
+                left++;
+            }
+
+            int windowSize = right - left + 1;
+
+            if (windowSize > maxWindow) {
+                maxWindow = windowSize;
+            }
+        }
+
+        System.out.println(n - maxWindow);
+    }
+}
